f7fb989ea79a7dcde142bda41c70db25dc8a11bb
hamadakuun <hamadakuun@gmail.com>
2022-06-10 11:45:46 +0900

    はじめてのコミット

---
+++ src/views/TestShow.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="test">
+        <h1>Test</h1>
+        <div>{{ test.title }}</div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        id: {
+            type: String,
+            required: true,
+        }
+    },
+    data() {
+        return {
+            tests: [
+                {
+                    id:1,
+                    title: 'Test1'
+                },
+                {
+                    id:2,
+                    title: 'Test2'
+                }
+            ]
+        }
+    },
+     computed: {
+        test(){
+            return this.tests.find(test => test.id == this.id)
+            }
+        },
+  mounted(){
+    console.log(this.$route)
+  }
+}
+</script>
---
+++ src/views/TestView.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="test">
+        <h1>Test</h1>
+        <div v-for="test in tests" :key="test.id">
+            <router-link :to="`/test/${test.id}`">
+                {{ test.title }}
+            </router-link>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      tests: [
+                {
+                    id: 1,
+                    title: 'Test1'
+                },
+                {
+                    id: 2,
+                    title: 'Test2'
+                }
+            ]
+        }
+    }
+}
+</script>
